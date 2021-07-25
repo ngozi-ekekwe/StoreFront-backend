35dc34693335e857339d2c3fdfb09773305435e9
Ngozi Ekekwe <ngozi-ekekwe>
2021-07-25 03:10:46 +0200

    fix broken client declaration

--- src/database.ts
+++ src/database.ts
@@ -3,23 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-let Client;
-
 const { DB_DEV, NODE_ENV, DB_TEST } = process.env;
-if (NODE_ENV === "test") {
-  Client = new Pool({
-    host: "127.0.0.1",
-    database: DB_DEV,
-    port: 5432,
-  });
-}
 
-if (NODE_ENV === "development") {
-  Client = new Pool({
-    host: "127.0.0.1",
-    database: DB_TEST,
-    port: 5432,
-  });
-}
+const Client = new Pool({
+  host: "127.0.0.1",
+  database: NODE_ENV === "test" ? DB_TEST : DB_DEV,
+  port: 5432,
+});
 
 export default Client;
