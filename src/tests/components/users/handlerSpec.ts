a66efdf7d3b07a3f6bdeab5eeafb9220ac2706ae
Ngozi Ekekwe <ngozi-ekekwe>
2021-07-25 05:57:09 +0200

    test works

--- src/tests/components/users/handlerSpec.ts
+++ src/tests/components/users/handlerSpec.ts
@@ -3,6 +3,31 @@ import app from "../../../app";
 
 const request = supertest(app);
 
-describe("/GET", () => {});
+const BASE_URL = "/api/v1/";
 
-describe("/POST", () => {});
+let token;
+
+describe("/POST", () => {
+  it("should create a new user and return a token", async () => {
+    const url = BASE_URL + "users";
+    const response = await request.post(url).send({
+      user: {
+        firstName: "ngozi",
+        lastName: "rose",
+        password: "password",
+      },
+    });
+    token = response.body.token;
+    expect(response.body.token).toBeDefined();
+    expect(response.body.success).toBe(true);
+  });
+});
+
+describe("/GET", () => {
+  it("should require a token to view all users", () => {
+    const url = BASE_URL + "users";
+    return request.get(url).expect(401);
+  });
+
+  it("should get a single user", () => {});
+});
