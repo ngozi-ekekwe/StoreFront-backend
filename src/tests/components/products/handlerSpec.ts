6e13a90284ba2ffa08dc2d6e8ea6d5d0df153a85
Ngozi Ekekwe <ngozi-ekekwe>
2021-07-25 05:51:04 +0200

    products test

--- src/tests/components/products/handlerSpec.ts
+++ src/tests/components/products/handlerSpec.ts
@@ -3,12 +3,68 @@ import app from "../../../app";
 
 const request = supertest(app);
 
-describe("/GET", () => {
-  it("should return a single product", () => {});
+let token: String;
 
-  it("should list all products", () => {});
+const BASE_URL = "/api/v1/";
+
+beforeAll(async () => {
+  // create new user and get token
+  const url = BASE_URL + "users";
+  const response = await request.post(url).send({
+    user: {
+      firstName: "ngozi",
+      lastName: "rose",
+      password: "password",
+    },
+  });
+  token = response.body.token;
+});
+
+describe("/GET", () => {
+  it("should return a single product", async () => {
+    const url = BASE_URL + "products/1";
+    const response = await request.get(url);
+    expect(response.status).toBe(200);
+  });
+
+  it("should list all products", async () => {
+    const url = BASE_URL + "products";
+    const response = await request.get(url);
+    expect(response.body.length).toBeGreaterThan(0);
+    expect(response.status).toBe(200);
+  });
 });
 
 describe("/POST", () => {
-  it("should create product", () => {});
+  it("should create product", async () => {
+    const url = BASE_URL + "products";
+    const response = await request
+      .post(url)
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        product: {
+          name: "product1",
+          price: 20,
+          category: "drinks",
+        },
+      });
+    expect(response.body.name).toBe("product1");
+    expect(response.body.category).toBe("drinks");
+    expect(response.status).toBe(201);
+  });
+
+  it("should require a token for creating products", async () => {
+    const url = BASE_URL + "products";
+    const response = await request.post(url).send({
+      product: {
+        name: "product1",
+        price: 20,
+        category: "drinks",
+      },
+    });
+    expect(response.body.message).toBe(
+      "Unauthorized, Token required to access this route"
+    );
+    expect(response.status).toBe(401);
+  });
 });
